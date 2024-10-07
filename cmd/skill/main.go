20cfdbbcb480e00e9e111eaa7e591c6f1cbde154
hairutdin <hairutdinaliev@gmail.com>
2024-10-07 17:30:47 +0300

    Added compression gzip

--- cmd/skill/main.go
+++ cmd/skill/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strings"
 
 	"go.uber.org/zap"
 
@@ -16,13 +17,41 @@ func main() {
 	}
 }
 
+func gzipMiddleware(h http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		ow := w
+
+		acceptEncoding := r.Header.Get("Accept-Encoding")
+		supportsGzip := strings.Contains(acceptEncoding, "gzip")
+		if supportsGzip {
+			cw := newCompressWriter(w)
+			ow = cw
+			defer cw.Close()
+		}
+
+		contentEncoding := r.Header.Get("Content-Encoding")
+		sendsGzip := strings.Contains(contentEncoding, "gzip")
+		if sendsGzip {
+			cr, err := newCompressReader(r.Body)
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+			r.Body = cr
+			defer cr.Close()
+		}
+
+		h.ServeHTTP(ow, r)
+	}
+}
+
 func run() error {
 	if err := logger.Initialize(flagLogLevel); err != nil {
 		return err
 	}
 
 	logger.Log.Info("Running server", zap.String("address", flagRunAddr))
-	return http.ListenAndServe(flagRunAddr, logger.RequestLogger(webhook))
+	return http.ListenAndServe(flagRunAddr, logger.RequestLogger(gzipMiddleware(webhook)))
 }
 
 func webhook(w http.ResponseWriter, r *http.Request) {
